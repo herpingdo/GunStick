5da26cee4aaa0668a216e831b065a247c77715a2
herpingdo <derp@derpy.fr>
2013-03-21 11:47:11 -0400

    Fixed indenting, made code more efficient, rename package.

--- src/com/herpingdo/gunstick/GunStick.java
+++ src/com/herpingdo/gunstick/GunStick.java
@@ -1,14 +1,12 @@
-package me.herp.derp.GunStick;
+package com.herpingdo.gunstick;
 
-import net.minecraft.server.EntityFireball;
-import net.minecraft.server.EntityLiving;
+import net.minecraft.server.v1_5_R1.EntityLiving;
+import net.minecraft.server.v1_5_R1.EntitySmallFireball;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.CraftWorld;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_5_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_5_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -30,7 +28,7 @@ public class GunStick extends JavaPlugin implements Listener {
 		EntityLiving playerEntity = craftPlayer.getHandle();
 		Vector lookat = player.getLocation().getDirection().multiply(10);
 		Location loc = player.getLocation();
-		EntityFireball fball = new EntityFireball(((CraftWorld) player.getWorld()).getHandle(), playerEntity, lookat.getX(), lookat.getY(), lookat.getZ());
+		EntitySmallFireball fball = new EntitySmallFireball(((CraftWorld) player.getWorld()).getHandle(), playerEntity, lookat.getX(), lookat.getY(), lookat.getZ());
 		fball.locX = loc.getX() + (lookat.getX()/5.0) + 0.25;
 		fball.locY = loc.getY() + (player.getEyeHeight()/2.0) + 0.5;
 		fball.locZ = loc.getZ() + (lookat.getZ()/5.0);
@@ -39,45 +37,46 @@ public class GunStick extends JavaPlugin implements Listener {
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onInteract(PlayerInteractEvent e)
-    	{
+    {
 		Material material = e.getMaterial();
 		if(material == Material.STICK)
 		{
 			boolean hasAmmo = false;
+			boolean takeAmmo = true;
 			if (e.getAction() == Action.RIGHT_CLICK_AIR || (e.getAction() == Action.RIGHT_CLICK_BLOCK)) //So it does not shoot when you "hit"
 			{
 				Player p = e.getPlayer();
 				if (p.hasPermission("gunstick.shoot") || p.isOp())
 				{
-					Integer normInvSize = p.getInventory().getSize();
-	         		        Integer i = 0;
-	           			for (i=0; i<normInvSize; i++) 
-	        		        {
-	            				ItemStack item = p.getInventory().getItem(i);
-	            				if (item.toString().toLowerCase().contains("sulphur"))
-	            				{
-	            					hasAmmo = true;
-	            					break;
-	            				}
-	           			 }
-	           			 if (p.hasPermission("gunstick.infinite"))
-	           			 {
+					int normInvSize = p.getInventory().getSize();
+	           		for (int i = 0; i < normInvSize; i++) 
+	        		{
+	            			ItemStack item = p.getInventory().getItem(i);
+	            			if (item.toString().toLowerCase().contains("sulphur"))
+	            			{
 	            				hasAmmo = true;
-			         	 }
-	           			 if (hasAmmo || p.isOp())
+	            				break;
+	            			}
+	           		}
+	           		if (p.hasPermission("gunstick.infinite") || p.isOp())
+	           		{
+	            			hasAmmo = true;
+	            			takeAmmo = false;
+			        }
+	           		if (hasAmmo)
+			        {
+	            			if (takeAmmo)
 			            	{
-	            				if (!p.isOp())
-			            		{
-			            			p.getInventory().removeItem(new ItemStack(Material.SULPHUR, 1));
-	            					p.getInventory().setContents(p.getInventory().getContents());
-	            					p.updateInventory();
-	            				}
-						fireball(p);
-						hasAmmo = false;
-	           			 }
+			            		p.getInventory().removeItem(new ItemStack(Material.SULPHUR, 1));
+	            				p.getInventory().setContents(p.getInventory().getContents());
+	            				p.updateInventory();
+	            			}
+	            			fireball(p);
+	            			hasAmmo = false;
+	           		}
 			else
 			{
-				p.sendMessage("You may not shoot guns!");
+				p.sendMessage("[GS] You may not shoot guns!");
 			}
 		}
     }
