f85d0c7e3acac5bdec78327a69f9c172c79c17e7
herpingdo <derp@derpy.fr>
2013-03-21 13:53:05 -0400

    Basic framework to disable/enable GS.

--- src/com/herpingdo/gunstick/GunStick.java
+++ src/com/herpingdo/gunstick/GunStick.java
@@ -1,10 +1,19 @@
 package com.herpingdo.gunstick;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 import net.minecraft.server.v1_5_R1.EntityLiving;
 import net.minecraft.server.v1_5_R1.EntitySmallFireball;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_5_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_5_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
@@ -17,23 +26,81 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 public class GunStick extends JavaPlugin implements Listener {
+	
+	private ArrayList<String> gunners = null;
+	private File dataFolder = null;
+	private String dataFolderStr = "";
+	
 	@Override
 	public void onEnable() 
 	{
 		getServer().getPluginManager().registerEvents(this, this);
+		dataFolder = this.getDataFolder();
+		dataFolder.mkdirs();
+		dataFolderStr = dataFolder.getAbsolutePath();
+		try
+		{
+			this.loadGunners();
+		}
+		catch (Exception e) { }
 	}
-	public void fireball(Player player)
+	
+	private void loadGunners() throws Exception
 	{
-		CraftPlayer craftPlayer = (CraftPlayer) player;
-		EntityLiving playerEntity = craftPlayer.getHandle();
-		Vector lookat = player.getLocation().getDirection().multiply(10);
-		Location loc = player.getLocation();
-		EntitySmallFireball fball = new EntitySmallFireball(((CraftWorld) player.getWorld()).getHandle(), playerEntity, lookat.getX(), lookat.getY(), lookat.getZ());
-		fball.locX = loc.getX() + (lookat.getX()/5.0) + 0.25;
-		fball.locY = loc.getY() + (player.getEyeHeight()/2.0) + 0.5;
-		fball.locZ = loc.getZ() + (lookat.getZ()/5.0);
-		((CraftWorld) player.getWorld()).getHandle().addEntity(fball);
+		this.gunners = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(new File(dataFolderStr, "gunners.txt")));
+		String line;
+		while ((line = br.readLine()) != null)
+		{
+			gunners.add(line);
+		}
+		br.close();
 	}
+	
+	private void saveGunners()
+	{
+		try
+		{
+			BufferedWriter br = new BufferedWriter(new FileWriter(new File(dataFolderStr, "gunners.txt")));
+			for (String s : gunners)
+			{
+				br.write(s);
+				br.newLine();
+				br.flush();
+			}
+			br.close();
+		} catch (Exception e) { }
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if (args.length == 2)
+		{
+			String operated = args[1];
+			if (args[0].equalsIgnoreCase("add"))
+			{
+				this.gunners.add(operated);
+				sender.sendMessage("GunStick enabled for player "+operated+"!");
+			}
+			else if (args[0].equalsIgnoreCase("del"))
+			{
+				this.gunners.remove(operated);
+				sender.sendMessage("GunStick disabled for player "+operated+"!");
+			}
+			else
+			{
+				sender.sendMessage("Usage: /gs <add/remove> <player>");
+			}
+		}
+		else
+		{
+			sender.sendMessage("Usage: /gs <add/remove> <player>");
+		}
+		return false;
+	}
+	
+
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onInteract(PlayerInteractEvent e)
@@ -74,11 +141,25 @@ public class GunStick extends JavaPlugin implements Listener {
 	            			fireball(p);
 	            			hasAmmo = false;
 	           		}
-			else
-			{
-				p.sendMessage("[GS] You may not shoot guns!");
+				}
+				else
+				{
+					p.sendMessage("[GS] You may not shoot guns!");
+				}
 			}
-		}
-    }
+		}	
+	}
+		
+	public void fireball(Player player)
+	{
+		CraftPlayer craftPlayer = (CraftPlayer) player;
+		EntityLiving playerEntity = craftPlayer.getHandle();
+		Vector lookat = player.getLocation().getDirection().multiply(10);
+		Location loc = player.getLocation();
+		EntitySmallFireball fball = new EntitySmallFireball(((CraftWorld) player.getWorld()).getHandle(), playerEntity, lookat.getX(), lookat.getY(), lookat.getZ());
+		fball.locX = loc.getX() + (lookat.getX()/5.0) + 0.25;
+		fball.locY = loc.getY() + (player.getEyeHeight()/2.0) + 0.5;
+		fball.locZ = loc.getZ() + (lookat.getZ()/5.0);
+		((CraftWorld) player.getWorld()).getHandle().addEntity(fball);
+	}
 }
-    }   }
